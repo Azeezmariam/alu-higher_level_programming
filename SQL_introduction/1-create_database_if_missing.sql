142e2e67f85b564a226d141c12bb9e9f1f73bd76
Azeezmariam <m.azeez@alustudent.com>
2023-03-30 07:56:09 -0700

    1-create_database_if_missing.sql

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,2 @@
--- yes
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+-- new tables
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
