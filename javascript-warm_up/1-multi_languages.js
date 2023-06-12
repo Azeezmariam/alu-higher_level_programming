135064628192a1cc1de990dfd3c0a16181cea3e8
Azeezmariam <m.azeez@alustudent.com>
2023-06-12 22:20:53 +0000

    multi

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,2 +1,5 @@
 #!/usr/bin/node
-console.log('C is fun\nPython is cool\nJavaScript is amazing');
+
+const lines = ['JavaScript is amazing', 'Python is cool', 'C is fun'];
+
+for (const line of lines.reverse()) console.log(line);
